761eff2a33e2334d73da9981bf139084afdf9254
ZoeyZhu2 <133013012+ZoeyZhu2@users.noreply.github.com>
2026-08-05 11:30:41 -0400

    Added update_folds as helper

--- cli.py
+++ cli.py
@@ -9,109 +9,80 @@ def main():
 
     poker_game = game.PokerGame(own_hand, own_position, num_players, [])
     while new_round:
-        print(f"Current equity: {poker_game.get_equity()}")
-        folds = input("Who has folded before you (BTN SB BB...): ").split()
-        for fold in folds:
-            poker_game.fold_player(fold)
-        print(f"Current equity: {poker_game.get_equity()}")
-        fold = input("Did you fold (Y/N): ").strip()
-        if fold == "Y":
-            next_str = input("Next round (Y/N): ").strip()
-            if next_str == "N":
-                print("Game ending!")
-                new_round = False
-                return
-            else:
-                print("Starting next round!")
-                own_hand = input("Your hand (format: Ah Kd): ").split()
-                num_players = int(input("Number of players (including you): "))
-                poker_game.new_round(own_hand, num_players)
-                continue
-        else:
-            folds = input("Who has folded after you (BTN SB BB...): ").split()
-            for fold in folds:
-                poker_game.fold_player(fold)
-            print(f"Current equity: {poker_game.get_equity()}")
+        status = update_folds(poker_game)
+        if status == "quit":
+            new_round = False
+            print("Game over!")
+            break
+        elif status == "fold":
+            print("Starting next round!")
+            own_hand = input("Your hand (format: Ah Kd): ").split()
+            num_players = int(input("Number of players (including you): "))
+            poker_game.new_round(own_hand, num_players)
+            break
         
         board_cards = input("Flop dealt (format: Ah Kd Qs): ").split()
         for card in board_cards:
             poker_game.add_board_card(card)
         
-        print(f"Current equity: {poker_game.get_equity()}")
-        folds = input("Who has folded (BTN SB BB...): ").split()
-        for fold in folds:
-            poker_game.fold_player(fold)
-        print(f"Current equity: {poker_game.get_equity()}")
-        fold = input("Did you fold (Y/N): ").strip()
-        if fold == "Y":
-            next_str = input("Next round (Y/N): ").strip()
-            if next_str == "N":
-                print("Game ending!")
-                new_round = False
-                return
-            else:
-                print("Starting next round!")
-                own_hand = input("Your hand (format: Ah Kd): ").split()
-                num_players = int(input("Number of players (including you): "))
-                poker_game.new_round(own_hand, num_players)
-                continue
-        else:
-            folds = input("Who has folded after you (BTN SB BB...): ").split()
-            for fold in folds:
-                poker_game.fold_player(fold)
-            print(f"Current equity: {poker_game.get_equity()}")
+        status = update_folds(poker_game)
+        if status == "quit":
+            new_round = False
+            print("Game over!")
+            break
+        elif status == "fold":
+            print("Starting next round!")
+            own_hand = input("Your hand (format: Ah Kd): ").split()
+            num_players = int(input("Number of players (including you): "))
+            poker_game.new_round(own_hand, num_players)
+            break
         
         board_card = input("Turn dealt (format: Ah): ")
         poker_game.add_board_card(board_card)
 
-        print(f"Current equity: {poker_game.get_equity()}")
-        folds = input("Who has folded before you (BTN SB BB...): ").split()
-        for fold in folds:
-            poker_game.fold_player(fold)
-        print(f"Current equity: {poker_game.get_equity()}")
-        fold = input("Did you fold (Y/N): ").strip()
-        if fold == "Y":
-            next_str = input("Next round (Y/N): ").strip()
-            if next_str == "N":
-                print("Game ending!")
-                new_round = False
-                return
-            else:
-                print("Starting next round!")
-                own_hand = input("Your hand (format: Ah Kd): ").split()
-                num_players = int(input("Number of players (including you): "))
-                poker_game.new_round(own_hand, num_players)
-                continue
-        else:
-            folds = input("Who has folded after you (BTN SB BB...): ").split()
-            for fold in folds:
-                poker_game.fold_player(fold)
-            print(f"Current equity: {poker_game.get_equity()}")
+        status = update_folds(poker_game)
+        if status == "quit":
+            new_round = False
+            print("Game over!")
+            break
+        elif status == "fold":
+            print("Starting next round!")
+            own_hand = input("Your hand (format: Ah Kd): ").split()
+            num_players = int(input("Number of players (including you): "))
+            poker_game.new_round(own_hand, num_players)
+            break
 
         board_card = input("River dealt: (format: Ah)")
-        print(f"Current equity: {poker_game.get_equity()}")
-        folds = input("Who has folded before you (BTN SB BB...): ").split()
+        status = update_folds(poker_game)
+        if status == "quit":
+            new_round = False
+            print("Game over!")
+            break
+        elif status == "fold":
+            print("Starting next round!")
+            own_hand = input("Your hand (format: Ah Kd): ").split()
+            num_players = int(input("Number of players (including you): "))
+            poker_game.new_round(own_hand, num_players)
+            break
+
+def update_folds(poker_game):
+    print(f"Current equity: {poker_game.get_equity()}")
+    folds = input("Who has folded before you (BTN SB BB...): ").split()
+    for fold in folds:
+        poker_game.fold_player(fold)
+    print(f"Current equity: {poker_game.get_equity()}")
+    fold = input("Did you fold (Y/N): ").strip()
+    if fold == "Y":
+        next_str = input("Next round (Y/N): ").strip()
+        if next_str == "N":
+            return "quit"
+        else:
+            return "fold"
+    else:
+        folds = input("Who has folded after you (BTN SB BB...): ").split()
         for fold in folds:
             poker_game.fold_player(fold)
         print(f"Current equity: {poker_game.get_equity()}")
-        fold = input("Did you fold (Y/N): ").strip()
-        if fold == "Y":
-            next_str = input("Next round (Y/N): ").strip()
-            if next_str == "N":
-                print("Game ending!")
-                new_round = False
-                return
-            else:
-                print("Starting next round!")
-                own_hand = input("Your hand (format: Ah Kd): ").split()
-                num_players = int(input("Number of players (including you): "))
-                poker_game.new_round(own_hand, num_players)
-                continue
-        else:
-            folds = input("Who has folded after you (BTN SB BB...): ").split()
-            for fold in folds:
-                poker_game.fold_player(fold)
-            print(f"Current equity: {poker_game.get_equity()}")
-
-        
-
+    
+def handle_status():
+    pass
