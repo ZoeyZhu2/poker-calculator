e381bf821a294cf383b3d5fc3486de4435145748
ZoeyZhu2 <133013012+ZoeyZhu2@users.noreply.github.com>
2026-08-05 12:42:22 -0400

    Made code less repetitive

--- cli.py
+++ cli.py
@@ -2,68 +2,35 @@
 import game
 
 def main():
-    new_round = True
     own_hand = input("Your hand (format: Ah Kd): ").split()
     num_players = int(input("Number of players (including you): "))
     own_position = input("Your position (BTN, SB, BB, UTG, UTG+1, UTG+2, LJ, HJ, CO): ")
 
     poker_game = game.PokerGame(own_hand, own_position, num_players, [])
-    while new_round:
-        status = update_folds(poker_game)
-        if status == "quit":
-            new_round = False
-            print("Game over!")
-            break
-        elif status == "fold":
-            print("Starting next round!")
-            own_hand = input("Your hand (format: Ah Kd): ").split()
-            num_players = int(input("Number of players (including you): "))
-            poker_game.new_round(own_hand, num_players)
+    while True:
+        if not handle_status(update_folds(poker_game), poker_game):
             break
         
         board_cards = input("Flop dealt (format: Ah Kd Qs): ").split()
         for card in board_cards:
             poker_game.add_board_card(card)
         
-        status = update_folds(poker_game)
-        if status == "quit":
-            new_round = False
-            print("Game over!")
-            break
-        elif status == "fold":
-            print("Starting next round!")
-            own_hand = input("Your hand (format: Ah Kd): ").split()
-            num_players = int(input("Number of players (including you): "))
-            poker_game.new_round(own_hand, num_players)
+        if not handle_status(update_folds(poker_game), poker_game):
             break
         
         board_card = input("Turn dealt (format: Ah): ")
         poker_game.add_board_card(board_card)
 
-        status = update_folds(poker_game)
-        if status == "quit":
-            new_round = False
-            print("Game over!")
-            break
-        elif status == "fold":
-            print("Starting next round!")
-            own_hand = input("Your hand (format: Ah Kd): ").split()
-            num_players = int(input("Number of players (including you): "))
-            poker_game.new_round(own_hand, num_players)
+        if not handle_status(update_folds(poker_game), poker_game):
             break
 
         board_card = input("River dealt: (format: Ah)")
-        status = update_folds(poker_game)
-        if status == "quit":
-            new_round = False
-            print("Game over!")
+
+        if not handle_status(update_folds(poker_game), poker_game):
             break
-        elif status == "fold":
-            print("Starting next round!")
-            own_hand = input("Your hand (format: Ah Kd): ").split()
-            num_players = int(input("Number of players (including you): "))
-            poker_game.new_round(own_hand, num_players)
-            break
+
+        own_position = poker_game.get_own_pos()
+        
 
 def update_folds(poker_game):
     print(f"Current equity: {poker_game.get_equity()}")
@@ -84,5 +51,14 @@ def update_folds(poker_game):
             poker_game.fold_player(fold)
         print(f"Current equity: {poker_game.get_equity()}")
     
-def handle_status():
-    pass
+def handle_status(status, poker_game):
+    if status == "quit":
+        print("Game over!")
+        return False
+    elif status == "fold":
+        print("Starting next round!")
+        own_hand = input("Your hand (format: Ah Kd): ").split()
+        num_players = int(input("Number of players (including you): "))
+        poker_game.new_round(own_hand, num_players)
+        return False
+    return True
