b5c75c266574f2887020ce986d37f5337afbeb92
ZoeyZhu2 <133013012+ZoeyZhu2@users.noreply.github.com>
2026-08-22 22:15:12 -0400

    Resume another day

--- bot.py
+++ bot.py
@@ -2,12 +2,18 @@ import random
 
 class bot():
 
-    def __init__(self, volatility, aggressiveness, stack):
+    def __init__(self, volatility, aggressiveness, looseness, stack):
         # volatility is a float from 0 to 1 measure percentage it can swing from playing according to target ev based on big_blind
-        # aggressiveness is a float from 0 to 1 which is min equity needed to raise
+        # aggressiveness to be figured out
+        # looseness is a percentage of default range (starting from best hands) it will play (can also go over default range)
         self.volatility = volatility
         self.aggressiveness = aggressiveness
+        self.looseness = looseness
         self.stack = stack
+
+    def get_equity(self, position):
+        # calculate range equity (looseness plays into effect here)
+        pass 
 
     def decision(self, equity, pot, cost_to_call, big_blind):
         if self.stack < 0:
@@ -22,12 +28,7 @@ class bot():
         adjusted_ev = ev + noise
         if adjusted_ev <= 0:
             return "fold", 0
-        elif equity > self.aggressiveness:
-            raise_amount = pot * 0.66 # standard for now
-            if raise_amount > self.stack:
-                raise_amount = self.stack
-            self.stack -= raise_amount
-            return "raise", raise_amount
+        # figure out raise behavior?
         else:
             self.stack -= cost_to_call
             return "call", cost_to_call
