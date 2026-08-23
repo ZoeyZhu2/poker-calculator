de68141d02f7e56cb6a963c866f22fbe375ab4a0
ZoeyZhu2 <133013012+ZoeyZhu2@users.noreply.github.com>
2026-08-23 01:28:57 -0400

    Updated seats and rotations

--- game.py
+++ game.py
@@ -1,104 +1,138 @@
 import position_ranges
 import equity_calculator
 import random
+import numpy as np
+import pot
 
 
 positions = ["BTN", "SB", "BB", "UTG", "UTG+1", "UTG+2", "LJ", "HJ", "CO"]
 
-class PokerGame:
-    def __init__(self, own_hand, own_pos, num_players, board_cards):
-        self.own_hand = own_hand
-        self.own_pos = own_pos
-        self.num_players = num_players
-        self.board_cards = board_cards
-        self.populate_positions(num_players)
-        self.folded = set()
 
+class PokerGame:
+    def __init__(self, own_hand: list, own_pos: str, own_seat: int, occupied_seats: list, board_cards: list):
+        # own_hand is a list of two cards ex: ["Ah", "Kd"]
+        self.own_hand = own_hand
+        # own_pos is a string representing position ex: "BTN"
+        self.own_pos = own_pos
+        # own_seat is an integer representing seat number
+        self.own_seat = own_seat
+        # occupied_seats is a list of integers
+        self.occupied_seats = occupied_seats
+        self.num_players = len(occupied_seats)
+        # board cards is a list of any community cards already dealt out ex: ["2s", "7d", "5h"]
+        self.board_cards = board_cards
+        self.populate_positions(occupied_seats)
+        # folded keeps track of folded cards. Not used right now.
+        self.folded = set()
+        # pot keeps track of the pot
+        self.pot = pot.Pot()
+        # seat -> bot for mode 1
+        self.bots = {1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None}
+        # seat -> looseness for mode 2 (eventually volatility and aggressiveness will be added)
+        self.traits = {1: 1.0, 2: 1.0, 3: 1.0, 4: 1.0, 5: 1.0, 6: 1.0, 7: 1.0, 8: 1.0, 9: 1.0}
+        
+    def update_traits(self, seat: int, looseness):
+        self.traits[seat] = looseness
+
+    # getting range equities (so an estimation)
     def get_equity(self):
         heads_up = (self.num_players == 2)
-        ranges = dict()
-        for key, value in self.positions_in.items():
+        ranges = dict() # position -> all range hands
+        for key, value in self.seats_in.items():
+            if key == self.own_seat:
+                continue
             if value is True:
-                ranges[key] = position_ranges.get_range_hands(key, heads_up=heads_up)
-        pocket_list = [self.own_hand]
-        selected_cards = set()
-        for key, value in ranges.items():
-            random_card = random.choice(value)
-            random_card_tuple = tuple(random_card)
-            if random_card_tuple not in selected_cards:
-                selected_cards.add(random_card_tuple)
-                pocket_list.append(random_card)
-
-        print(f"pocket_list: {pocket_list}")  # Debug
-        print(f"board: {self.board_cards}")  # Debug
-        print(f"pocket_list[0] type: {type(pocket_list[0])}")  # ADD THIS
-        print(f"pocket_list[1] type: {type(pocket_list[1])}")  # ADD THIS
-
-        equity_list = equity_calculator.calculate_equity(pocket_list, self.board_cards, 10000)
-        return equity_list[0]
+                pos = self.rotation[key]
+                ranges[pos] = position_ranges.get_range_hands(pos, heads_up=heads_up, looseness=self.traits[key])
+        equities_sum = np.zeros(len(ranges) + 1)
+        for i in range(10000):
+            pocket_list = [self.own_hand]
+            selected_cards = set()
+            for card in self.own_hand:
+                selected_cards.add(card)
+            for card in self.board_cards:
+                selected_cards.add(card)
+            for key, value in ranges.items():
+                random_cards = random.choice(value)
+                card1, card2 = random_cards
+                while card1 in selected_cards or card2 in selected_cards:
+                    random_cards = random.choice(value)
+                    card1, card2 = random_cards
+                selected_cards.add(card1)
+                selected_cards.add(card2)
+                pocket_list.append(random_cards)
+            # only testing one combination of community cards per combinations of hands:
+            equity_list = equity_calculator.calculate_equity(pocket_list, self.board_cards, 1)
+            equities_sum = equities_sum + np.array(equity_list)
+        avg_equities = equities_sum / 10000    
+        return avg_equities[0]
     
-    def fold_player(self, player_pos):
-        self.positions_in[player_pos] = False
+    def fold_player(self, seat):
+        self.seats_in[seat] = False
+
+    def player_bet(self, seat, amount):
+        self.pot.add_contribution(seat, amount)
 
     def add_board_card(self, card):
         self.board_cards.append(card)
 
-    def new_round(self, own_hand, num_players):
-        # change everyone's positions by one
+    def get_payout(self, player_cards):
+        return self.pot.calculate_payout(player_cards, self.board_cards)
+
+    def new_round(self, own_hand, seats_occupied, own_seat=None):
+        if own_seat is not None:
+            self.own_seat = own_seat
         self.own_hand = own_hand
-        self.num_players = num_players
-        self.populate_positions(num_players)
+        self.num_players = len(seats_occupied)
+        self.populate_positions(seats_occupied, rotate=True)
+        self.pot = pot.Pot()
 
 
-    def populate_positions(self, num_players):
-        self.positions_in = {"UTG": False, "UTG+1": False, "UTG+2": False, "LJ": False, "HJ": False, "CO": False, "BTN": True, "SB": False, "BB": True}
+    def populate_positions(self, seats_occupied, rotate=False):
+        # position -> in game? for both modes
+        self.seats_in = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False}
+        for num in seats_occupied:
+            self.seats_in[num] = True
+        num_players = len(seats_occupied)
+        active_pos = set()
+        if num_players > 1:
+            active_pos.add("BTN")
+            active_pos.add("BB")
+            # this is so ranges are more accurate in heads-up
         if num_players > 2:
-            self.positions_in["SB"] = True
+            active_pos.add("SB")
         if num_players > 3:
-            self.positions_in["UTG"] = True
-        if num_players > 4: 
-            self.positions_in["CO"] = True
-        if num_players > 5: 
-            self.positions_in["HJ"] = True
-        if num_players > 6: 
-            self.positions_in["LJ"] = True
-        if num_players > 7: 
-            self.positions_in["UTG+1"] = True
-        if num_players > 8: 
-            self.positions_in["UTG+2"] = True
-        self.own_pos = positions[(positions.index(self.own_pos) + 1) % 9]
-        while self.positions_in[self.own_pos] == False:
-            self.own_pos = positions[(positions.index(self.own_pos) + 1) % 9]
-        self.board_cards.clear()
+            active_pos.add("UTG")
+        if num_players > 4:
+            active_pos.add("CO")
+        if num_players > 5:
+            active_pos.add("HJ")
+        if num_players > 6:
+            active_pos.add("LJ")
+        if num_players > 7:
+            active_pos.add("UTG+1")
+        if num_players > 8:
+            active_pos.add("UTG+2")
+        in_positions = [pos for pos in positions if pos in active_pos]
+        if rotate == True:
+            self.own_pos = in_positions[(in_positions.index(self.own_pos) + 1) % num_players]
+        # seat -> position for both modes
+        self.rotation = {self.own_seat: self.own_pos}
+        own_pos_idx = in_positions.index(self.own_pos)
+        next_pos_idx = (own_pos_idx + 1) % num_players
+        own_seat_idx = seats_occupied.index(self.own_seat)
+        next_seat_idx = (own_seat_idx + 1) % num_players
+        while next_seat_idx != own_seat_idx:
+            self.rotation[seats_occupied[next_seat_idx]] = in_positions[next_pos_idx]
+            next_pos_idx = (next_pos_idx + 1) % num_players
+            next_seat_idx = (next_seat_idx + 1) % num_players
 
-    def get_positions_in(self):
-        return self.positions_in
+
+
+
+    def get_seats_in(self):
+        return self.seats_in
 
     def get_own_pos(self):
         return self.own_pos
 
-
-# Test the CLI game
-
-# game = PokerGame(['Ah', 'Kh'], 'BTN', 3, [])
-# print(f"3 players, BTN position, no board")
-# print(f"Your hand: AKs")
-# print(f"Equity: {game.get_equity():.2%}")
-
-# game.add_board_card('5h')
-# game.add_board_card('5c')
-# game.add_board_card('5d')
-# print(f"\nAdded flop: 5h 5c 5d")
-# print(f"Equity: {game.get_equity():.2%}")
-
-# game.fold_player('SB')
-# print(f"\nSB folded (2 players left)")
-# print(f"Equity: {game.get_equity():.2%}")
-
-# game.add_board_card('2s')
-# print(f"\nAdded turn: 2s")
-# print(f"Equity: {game.get_equity():.2%}")
-
-# game.add_board_card('3c')
-# print(f"\nAdded river: 3c")
-# print(f"Equity: {game.get_equity():.2%}")
