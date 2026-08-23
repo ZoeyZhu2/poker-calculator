d6f1b1dd6ef0a2b6f71bf10c38024fae707a0390
ZoeyZhu2 <133013012+ZoeyZhu2@users.noreply.github.com>
2026-08-23 01:28:38 -0400

    Minor Notes

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ class bot():
 
     def __init__(self, volatility, aggressiveness, looseness, stack):
         # volatility is a float from 0 to 1 measure percentage it can swing from playing according to target ev based on big_blind
-        # aggressiveness to be figured out
+        # aggressiveness governs raising and calling
         # looseness is a percentage of default range (starting from best hands) it will play (can also go over default range)
         self.volatility = volatility
         self.aggressiveness = aggressiveness
