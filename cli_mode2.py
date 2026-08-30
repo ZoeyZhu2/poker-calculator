e7d38965140a400ccb139ba5ce41b3d7d2d112f8
ZoeyZhu2 <133013012+ZoeyZhu2@users.noreply.github.com>
2026-08-30 01:43:42 -0400

    Check/Bet/Fold but no all-in accomodations yet

--- cli_mode2.py
+++ cli_mode2.py
@@ -17,21 +17,25 @@ def main():
         poker_game = game.PokerGame(own_hand, own_pos, own_seat, occupied_seats, [])
 
         while True:
-            update_action(poker_game, 0)
-            
-            board_cards_str = get_input("Flop dealt (format: AhKdQs): ")
-            for i in range(0, len(board_cards_str)-2,2):
-                poker_game.add_board_card(board_cards_str[i:i+2])
-            
-            update_action(poker_game, 1)
-            
-            board_card = get_input("Turn dealt (format: Ah): ")
-            poker_game.add_board_card(board_card)
+            num_in = update_action(poker_game, 0)
 
-            update_action(poker_game, 2)
-            board_card = get_input("River dealt: (format: Ah)")
+            if num_in > 1:
+                board_cards_str = get_input("Flop dealt (format: AhKdQs): ")
+                for i in range(0, len(board_cards_str)-2,2):
+                    poker_game.add_board_card(board_cards_str[i:i+2])
+                
+                num_in = update_action(poker_game, 1)
 
-            update_action(poker_game, 3)
+            if num_in > 1:
+                board_card = get_input("Turn dealt (format: Ah): ")
+                poker_game.add_board_card(board_card)
+
+                num_in = update_action(poker_game, 2)
+
+            if num_in > 1:
+                board_card = get_input("River dealt: (format: Ah)")
+                poker_game.add_board_card(board_card)
+                num_in = update_action(poker_game, 3)
             
             next_round = get_input("Next round (Y/N): ").strip()
             if next_round == "N":
@@ -63,18 +67,38 @@ def update_action(poker_game, betting_round):
     # betting_round: 0,1,2,3 for preflop, post flop, post turn, post river
     current_equity = poker_game.get_equity()
     print(f"Current equity: {current_equity}")
-    action_order = poker_game.get_action_order(betting_round)
-    for seat in action_order:
-        if seat == poker_game.get_own_seat():
-            cost = poker_game.get_max_contribution() - poker_game.get_own_contribution()
-            print(f"Current EV: {poker_game.calculate_ev(current_equity, cost)}")
-        amount = int(get_input(f"How much did the player in seat {seat} bet? (0 for fold): "))
-        if amount == 0:
-            poker_game.fold_player(seat)
-            current_equity = poker_game.get_equity()
-            print(f"Current equity: {current_equity}")
-        else:
-            poker_game.player_bet(seat, amount)    
+    left_to_act = poker_game.get_num_in()
+    while left_to_act > 0: # until all bets are equal or all ins or whatever (need to write break thing)
+        action_order = poker_game.get_action_order(betting_round)
+        for seat in action_order:
+            curr_max_contribution = poker_game.get_max_contribution()
+            if seat == poker_game.get_own_seat():
+                cost_to_call = poker_game.get_max_contribution() - poker_game.get_own_contribution()
+                print(f"Current EV to call: {poker_game.calculate_ev_call(current_equity, cost_to_call)}")
+                while True:
+                    bet = get_input("Enter how much you want to raise to calculate new EV. Enter 0 to stop.")
+                    if bet.strip() == "0":
+                        break
+                    bet = int(bet)
+                    if bet < cost_to_call:
+                        print("You must input a higher number")
+                        continue
+                    print(f"Current EV to raise: {poker_game.calculate_ev_raise(bet)}")
+            amount = int(get_input(f"How much did the player in seat {seat} bet? (any negative for fold): "))
+            if amount < 0:
+                poker_game.fold_player(seat)
+                current_equity = poker_game.get_equity()
+                print(f"Current equity: {current_equity}")
+                if poker_game.get_num_in() == 1:
+                    return poker_game.get_num_in()
+            elif amount == 0:
+                pass # do nothing!
+            else:
+                poker_game.player_bet(seat, amount)
+            if poker_game.get_max_contribution() != curr_max_contribution:
+                left_to_act = poker_game.get_num_in()
+            left_to_act -= 1
+    return poker_game.get_num_in()
 
 if __name__ == "__main__": # name is only main if a user runs it, not if another file imports this one
     main()
